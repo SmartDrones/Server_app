6877ac9dd92a429095ee12e1222c02b457474510
jlucidar <jlucidar@gmail.com>
2015-02-08 11:18:18 +0100

    boum ! server ready for your drones !

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var restify = require('restify');
 var socketio = require('socket.io');
 
-log.info("Setting up the webserver...");
+console.log("Setting up the webserver...");
 var server = restify.createServer({name:"Smart Drones"});
 this.server = server;
 
 // Allow JSON over Cross-origin resource sharing 
-log.info("Configuring cross-origin requests...");
-server.use(
-    function crossOrigin(req,res,next){
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    }
-);
 
 server.listen(8080, function() {
-    log.info(server.name+ ' listening at '+ server.url);
+    console.log(server.name+ ' listening at '+ server.url);
 });
 
 // Define a route for serving static files
@@ -26,18 +19,21 @@ server.get(/.*/, restify.serveStatic({
 	default: 'index.html'
 }));
 
-
+var tab_clients = [];
 var clients_limit = 4;
 var nb_clients=0;
 
 function connect(socket) {
 
-	log.debug("new_client for the app");
+	console.log("new_client for the app");
 	nb_clients++;
 	if (nb_clients<=clients_limit){ // avoid too many connection on the server.
 		socket.emit("id",(socket.id));
+		tab_clients.push(socket.id);
+		socket.emit("drones_ids",tab_clients);
 		socket.on('client_order',function(data){
 				socket.emit("command",data);
+				console.log(data);
 //				for(var i in data.drone)
 //				{
 //					send_command(data.cmd,data.drone[i]);
@@ -46,16 +42,11 @@ function connect(socket) {
 
 	}
 	socket.on('disconnect', function() {
-		log.debug("client disconnected");
+		tab_clients.pop(socket.id);
+		console.log("client disconnected");
 		nb_clients--;
 	});
 }
-
-function send_command(data_cmd,id_drone)
-{
-
-}
-
 	var io = socketio.listen(server.server);
 	io.on('connection', connect);
 
